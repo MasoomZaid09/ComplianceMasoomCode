50f0dc1ebc7e290761776a7d0106a09ae0fe9c72
Vishal <smartboyvishal30@gmail.com>
2022-04-17 17:47:54 +0530

    added confiqure standard page

---
+++ app/src/main/java/com/example/complyanyproject/activity/ConfigureStandardActivity.kt
@@ -0,0 +1,13 @@
+package com.example.complyanyproject.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.complyanyproject.R
+
+class ConfigureStandardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configure_standard)
+    }
+}
