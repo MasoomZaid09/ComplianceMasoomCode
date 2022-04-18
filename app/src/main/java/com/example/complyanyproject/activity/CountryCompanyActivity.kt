ca504e91bcf4e1a254fed377895b67a9a1c5520d
Masoom Zaid <an-masoom@indicchain.com>
2022-04-18 18:04:04 +0530

    added company management code

--- app/src/main/java/com/example/complyanyproject/activity/CountryCompanyActivity.kt
+++ app/src/main/java/com/example/complyanyproject/activity/CountryCompanyActivity.kt
@@ -3,10 +3,26 @@ package com.example.complyanyproject.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.complyanyproject.R
+import com.example.complyanyproject.databinding.ActivityCountryCompanyBinding
+import com.example.complyanyproject.databinding.ActivityCountryRegionCompanyBinding
+import com.example.complyanyproject.databinding.ActivityGlobalCompanyBinding
 
 class CountryCompanyActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCountryCompanyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_country_company)
+        binding = ActivityCountryCompanyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.backCountryCompany.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 }
--- app/src/main/java/com/example/complyanyproject/activity/CountryRegionCompanyActivity.kt
+++ app/src/main/java/com/example/complyanyproject/activity/CountryRegionCompanyActivity.kt
@@ -3,10 +3,23 @@ package com.example.complyanyproject.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.complyanyproject.R
+import com.example.complyanyproject.databinding.ActivityCountryRegionCompanyBinding
 
 class CountryRegionCompanyActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCountryRegionCompanyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_country_region_company)
+        binding = ActivityCountryRegionCompanyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backCountryRegionCompany.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        finish()
     }
 }
