5165d5c0d14633c2de73dd9295acf4d12409be25
administrator <an-vishal@indicchain.com>
2022-04-20 17:24:13 +0530

    Final Commit (20 April, 2022)

--- app/src/main/java/com/example/complyanyproject/MenuFragments/DashboardFragment.kt
+++ app/src/main/java/com/example/complyanyproject/MenuFragments/DashboardFragment.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Spinner
 import com.example.complyanyproject.R
 import com.example.complyanyproject.activity.DetailsActivity
 import com.example.complyanyproject.activity.RegisterEmailNotificationActivity
@@ -15,6 +17,7 @@ import com.example.complyanyproject.activity.RegisterEmailNotificationActivity
 class DashboardFragment : Fragment() {
 
     lateinit var profileBtn : ImageView
+//    lateinit var spinnerDashboardFragment: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,13 +26,19 @@ class DashboardFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_dashboard, container, false)
 
+//        val compliance_options = resources.getStringArray(R.array.compliance_options)
+//        val arrayAdapter = ArrayAdapter(this, R.layout.fragment_dashboard)
+
         profileBtn = view.findViewById(R.id.toolbarProfileIcon)
+//        spinnerDashboardFragment = view.findViewById(R.id.spinnerDashboardFragment)
 
         profileBtn.setOnClickListener {
             Intent(activity as Context, DetailsActivity::class.java).also{
                 startActivity(it)
             }
         }
+
+
 
         return view
     }
