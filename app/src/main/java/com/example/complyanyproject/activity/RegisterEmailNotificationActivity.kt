a2205ed9fbae521bd5f5e9f822f8e6e794d8b137
Masoom Zaid <an-masoom@indicchain.com>
2022-04-21 13:05:37 +0530

    added recycler in RegisterEmailNotificationActivity.kt

--- app/src/main/java/com/example/complyanyproject/activity/RegisterEmailNotificationActivity.kt
+++ app/src/main/java/com/example/complyanyproject/activity/RegisterEmailNotificationActivity.kt
@@ -8,7 +8,11 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.complyanyproject.R
+import com.example.complyanyproject.adapter.RegisterEmailNotificationAdapter
+import com.example.complyanyproject.model.RegisterEmailModelClass
 import com.example.complyanyproject.selectorActivity.CompanySelectorActivity
 
 class RegisterEmailNotificationActivity : AppCompatActivity() {
@@ -17,6 +21,11 @@ class RegisterEmailNotificationActivity : AppCompatActivity() {
     lateinit var backButton : LinearLayout
     lateinit var imgFilter : ImageView
 
+    lateinit var recyclerView : RecyclerView
+    lateinit var adapter : RegisterEmailNotificationAdapter
+
+    val itemList : ArrayList<RegisterEmailModelClass> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_email_notification)
@@ -24,6 +33,7 @@ class RegisterEmailNotificationActivity : AppCompatActivity() {
         etSelectEntity = findViewById(R.id.etSelectEntity)
         backButton = findViewById(R.id.backButton)
         imgFilter = findViewById(R.id.imgFilter)
+        recyclerView = findViewById(R.id.recyclerViewRegisterEmail)
 
         etSelectEntity.setOnClickListener {
             Intent(this, CompanySelectorActivity::class.java).also{
@@ -38,6 +48,41 @@ class RegisterEmailNotificationActivity : AppCompatActivity() {
         imgFilter.setOnClickListener {
             onBackPressed()
         }
+
+
+        val data1 = RegisterEmailModelClass("testing")
+        val data2 = RegisterEmailModelClass("testing")
+        val data3 = RegisterEmailModelClass("testing")
+        val data4 = RegisterEmailModelClass("testing")
+        val data5 = RegisterEmailModelClass("testing")
+        val data6 = RegisterEmailModelClass("testing")
+        val data7 = RegisterEmailModelClass("testing")
+        val data8 = RegisterEmailModelClass("testing")
+        val data9 = RegisterEmailModelClass("testing")
+        val data10 = RegisterEmailModelClass("testing")
+        val data11 = RegisterEmailModelClass("testing")
+        val data12 = RegisterEmailModelClass("testing")
+
+        itemList.add(data1)
+        itemList.add(data12)
+        itemList.add(data2)
+        itemList.add(data3)
+        itemList.add(data4)
+        itemList.add(data5)
+        itemList.add(data6)
+        itemList.add(data7)
+        itemList.add(data8)
+        itemList.add(data9)
+        itemList.add(data1)
+        itemList.add(data10)
+        itemList.add(data11)
+        itemList.add(data12)
+
+
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = RegisterEmailNotificationAdapter(this, itemList)
+        recyclerView.adapter = adapter
 
     }
 
