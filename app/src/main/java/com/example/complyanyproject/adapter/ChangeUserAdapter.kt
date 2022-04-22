a85d30e353bc5d7ecb789273ce9b5a412760681d
Masoom Zaid <an-masoom@indicchain.com>
2022-04-22 17:38:03 +0530

    added clicks on user management recycler

--- app/src/main/java/com/example/complyanyproject/adapter/ChangeUserAdapter.kt
+++ app/src/main/java/com/example/complyanyproject/adapter/ChangeUserAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.complyanyproject.adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.complyanyproject.R
@@ -25,7 +27,51 @@ class ChangeUserAdapter(val context: Context, val itemList : ArrayList<ChangeUse
         holder.itemDepartment.text = userData.Department
         holder.itemUserName.text = userData.ReplaceUserName
 
-    }
+        holder.imgChangeBlock.setOnClickListener {
+
+            // added clicks on block
+            holder.imgChangeBlock.setOnClickListener {
+
+                if (holder.flag1 == true) {
+
+                    val dialog = AlertDialog.Builder(context)
+                    dialog.setTitle("Unblock")
+                    dialog.setMessage("Are you sure you want to Unblock?")
+                    dialog.setIcon(R.drawable.ic_block)
+                    dialog.setPositiveButton("Yes") { text, listener ->
+                        holder.imgChangeBlock.setImageDrawable(context.resources.getDrawable(R.drawable.ic_block))
+                    }
+                    dialog.setNegativeButton("No") { text, listener ->
+
+                    }
+                    dialog.create()
+                    dialog.show()
+
+                    holder.flag1 = false
+
+                } else {
+
+                    val dialog = AlertDialog.Builder(context)
+                    dialog.setTitle("Block")
+                    dialog.setMessage("Are you sure you want to Block?")
+                    dialog.setIcon(R.drawable.ic_baseline_block_24)
+                    dialog.setPositiveButton("Yes") { text, listener ->
+                        holder.imgChangeBlock.setImageDrawable(context.resources.getDrawable(R.drawable.ic_baseline_block_24))
+                    }
+                    dialog.setNegativeButton("No") { text, listener ->
+
+                    }
+                    dialog.create()
+                    dialog.show()
+
+                    holder.flag1 = true
+                }
+            }
+
+
+            }
+
+        }
 
     override fun getItemCount(): Int {
         return itemList.size
@@ -37,6 +83,9 @@ class ChangeUserAdapter(val context: Context, val itemList : ArrayList<ChangeUse
         val itemUser : TextView = view.findViewById(R.id.itemUser)
         val itemDepartment : TextView = view.findViewById(R.id.itemDepartment)
         val itemUserName : TextView = view.findViewById(R.id.itemUserName)
+        val imgChangeBlock :ImageView = view.findViewById(R.id.imgChangeBlock)
+        var flag1 = false
+
 
     }
 }
--- app/src/main/java/com/example/complyanyproject/adapter/ProxyUserAdapter.kt
+++ app/src/main/java/com/example/complyanyproject/adapter/ProxyUserAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.complyanyproject.adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.complyanyproject.R
@@ -26,6 +28,50 @@ class ProxyUserAdapter (val context: Context, val itemList : ArrayList<ProxyUser
         holder.itemEntityName.text = userData.EntityName
         holder.itemDepartment.text = userData.Department
         holder.itemEndDate.text = userData.ProxyEndDate
+
+        holder.imgProxyUser.setOnClickListener {
+
+            // added clicks on block
+            holder.imgProxyUser.setOnClickListener {
+
+                if (holder.flag == true) {
+
+                    val dialog = AlertDialog.Builder(context)
+                    dialog.setTitle("Unblock")
+                    dialog.setMessage("Are you sure you want to Unblock?")
+                    dialog.setIcon(R.drawable.ic_block)
+                    dialog.setPositiveButton("Yes") { text, listener ->
+                        holder.imgProxyUser.setImageDrawable(context.resources.getDrawable(R.drawable.ic_block))
+                    }
+                    dialog.setNegativeButton("No") { text, listener ->
+
+                    }
+                    dialog.create()
+                    dialog.show()
+
+                    holder.flag = false
+
+                } else {
+
+                    val dialog = AlertDialog.Builder(context)
+                    dialog.setTitle("Block")
+                    dialog.setMessage("Are you sure you want to Block?")
+                    dialog.setIcon(R.drawable.ic_baseline_block_24)
+                    dialog.setPositiveButton("Yes") { text, listener ->
+                        holder.imgProxyUser.setImageDrawable(context.resources.getDrawable(R.drawable.ic_baseline_block_24))
+                    }
+                    dialog.setNegativeButton("No") { text, listener ->
+
+                    }
+                    dialog.create()
+                    dialog.show()
+
+                    holder.flag = true
+                }
+
+
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -38,7 +84,9 @@ class ProxyUserAdapter (val context: Context, val itemList : ArrayList<ProxyUser
         val itemEntityName : TextView = view.findViewById(R.id.itemEntityName)
         val itemDepartment : TextView = view.findViewById(R.id.itemDepartment)
         val itemEndDate : TextView = view.findViewById(R.id.itemEndDate)
+        val imgProxyUser : ImageView = view.findViewById(R.id.imgProxyUser)
 
+        var flag = false
     }
 
 
